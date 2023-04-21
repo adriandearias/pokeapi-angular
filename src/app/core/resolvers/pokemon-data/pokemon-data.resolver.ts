0936db74d54a38e9a1ed49459dec15dfd72f697e
adriandearias <deariasadrian@gmail.com>
2023-04-21 21:59:51 +0200

    .

--- src/app/core/resolvers/pokemon-data/pokemon-data.resolver.ts
+++ src/app/core/resolvers/pokemon-data/pokemon-data.resolver.ts
@@ -1,28 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Router, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable, of, BehaviorSubject } from 'rxjs';
+import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { PokemonModelRaw } from '@core/models/pokemonModelRaw.model';
-import { take, filter, tap } from 'rxjs/operators';
 import { PokeapiRawDataService } from '@core/services/pokeapi-raw-data/pokeapi-raw-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonDataResolver implements Resolve<PokemonModelRaw[]> {
-  
-  private pokemonData$ = new BehaviorSubject<PokemonModelRaw[]>([]);
-
-  constructor(private router: Router, private pokeapiRawDataService: PokeapiRawDataService) { }
+  // private pokemonData$ = new BehaviorSubject<PokemonModelRaw[]>([]);
+  constructor(private pokeapiRawDataService: PokeapiRawDataService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PokemonModelRaw[]> {
-    if (this.pokemonData$.getValue().length) {
-      return this.pokemonData$.asObservable().pipe(take(1));
-    } else {
-      return this.pokeapiRawDataService.getPokemons().pipe(
-        filter(pokemonList => pokemonList.length > 0),
-        take(1),
-        tap(pokemonList => this.pokemonData$.next(pokemonList))
-      );
-    }
-  }
-}
+    console.log(this.pokeapiRawDataService.getPokemons());
+    return this.pokeapiRawDataService.getPokemons()
+}}
